a5c651b1074f5ec581a00602a17d964bade89d33
Tyrone Sta. Maria <tyrone.stamaria35@gmail.com>
2021-01-06 22:35:56 +0800

    90%

--- paimon.c
+++ paimon.c
@@ -51,7 +51,7 @@ struct Set* create (){
     
     set->next = NULL;
     set->isSet = 0;
-    printf("SHIT CREATED\n");
+    // printf("SHIT CREATED\n");
     
     return set;
 }
@@ -85,20 +85,13 @@ struct Set* add(struct Set* prev_set, int new_data)
         prev_set->isSet = 1;
         prev_set->next = new_node;
     }else{
-        /*
-        5
-        20
-        25
-        */
 
-        // int temp = 0;
-
-        // if(prev_set->data < new_data){
-            
-        // }
-
-
-        add(prev_set->next, new_data);
+        if(prev_set->data != new_data){
+            add(prev_set->next, new_data);
+        } 
+        else{
+            return prev_set; 
+        }
     }
 
     // sort dito 
@@ -108,30 +101,102 @@ struct Set* add(struct Set* prev_set, int new_data)
  
 }
 
-struct Set* unionSet
+struct Set* unionSet(struct Set* a, struct Set* b){
+    struct Set* s1 = create();
+    
+
+    while(a->isSet){
+        add(s1, a->data);
+        a = a->next;
+    }
+    
+    while(b->isSet){
+        add(s1, b->data);
+        b = b->next;
+    }
+
+    
+    return s1;
+}
+
+struct Set* intersectSet(struct Set* a, struct Set* b){
+    struct Set* s1 = create();
+    struct Set* b_start = b;
+    while(a->isSet){
+        while(b->isSet){
+            if (a->data == b->data){
+                add(s1, a->data);
+            }
+            b = b->next;
+        }
+        b = b_start;
+        a = a->next;
+    }
+    return s1;
+}
+
+
   
 void print (struct Set* set){
-    if (set->isSet)
+    if (set->isSet == 1){
+        printf ("%d ", set->data);
+        print (set->next);
+    }
+    else {
+        printf("\n");
+        return;
+    }
 }
+
 
 int main(){
 
-    struct Set* s1,* s2,* s3;
+    struct Set* s1,* s2,* s3, *s4;
 
     s1 = create();
+    s2 = create();
 
     //s1->data = 5;
-    add(s1, 5);
-    printf("hello %d\n",add(s1, 25)->data);
-    add(s1, 30);
-    printf("hello %d\n",add(s1, 20)->data);
+    // add(s1, 5);
+    // printf("hello %d\n",add(s1, 25)->data);
+    // add(s1, 30);
+    // printf("hello %d\n",add(s1, 20)->data);
     // add(s1, 25);
 
-    printf("%d\n", s1->data);
-    printf("%d\n", s1->next->data);
-    printf("%d\n", s1->next->next->data);
-    printf("%d\n", s1->next->next->next->data);
 
+    add(s1, 20);
+    add(s1, 20);
+    add(s1, 20);
+    add(s1, 25);
+    add(s1, 30);
+    add(s1, 20);
+    add(s1, 5);
+
+    add(s2, 212);
+    add(s2, 17);
+    add(s2, 18);
+    add(s2, 30);
+    add(s2, 32);
+    add(s2, 25);
+    add(s2, 6);
+
+
+    printf("UNION:\n");
+    print(unionSet(s1, s2));
+
+    printf("[%d]\n",add(s1, 30)->data);
+
+    printf("INTERSECT\n");
+    print(intersectSet(s1, s2));
+
+    // add(s1, 5);
+
+    // printf("%d\n", s1->data);
+    // printf("%d\n", s1->next->data);
+    // printf("%d\n", s1->next->next->data);
+    // printf("%d\n", s1->next->next->next->data);
+
+    print (s1);
 
 //    s2 = create();
 //    s3 = create();
